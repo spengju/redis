4f8b5ca7190db91f52c75a4c5dfe64be1a8e923b
spengju <2658111307@qq.com>
2024-09-30 01:34:05 +0800

    AppConfig

--- redis-multi-cache/src/main/java/com/peng/Application.java
+++ redis-multi-cache/src/main/java/com/peng/Application.java
@@ -1,17 +1,9 @@
 package com.peng;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.redisson.Redisson;
-import org.redisson.config.Config;
+import com.peng.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 
 /**
  * @Author: spengju
@@ -23,7 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AppConfig.class, args);
     }
 
 }
