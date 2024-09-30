4a872d2f6146587959cfe7d6c0d7c8a0db36a038
spengju <2658111307@qq.com>
2024-09-30 13:17:52 +0800

    布隆过滤器

--- redis-lock/src/main/java/com/peng/redission/filter/RedissonBloomFilter.java
+++ redis-lock/src/main/java/com/peng/redission/filter/RedissonBloomFilter.java
@@ -14,7 +14,7 @@ public class RedissonBloomFilter {
 
     public static void main(String[] args) {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setAddress("redis://111.229.94.137:6379");
         //构造Redisson
         RedissonClient redisson = Redisson.create(config);
 
@@ -22,12 +22,13 @@ public class RedissonBloomFilter {
         //初始化布隆过滤器：预计元素为100000000L,误差率为3%,根据这两个参数会计算出底层的bit数组大小
         bloomFilter.tryInit(100000L,0.03);
         //将zhuge插入到布隆过滤器中
-        bloomFilter.add("zhuge");
-        bloomFilter.add("tuling");
+        bloomFilter.add("peng");
+        bloomFilter.add("ju");
 
         //判断下面号码是否在布隆过滤器中
-        System.out.println(bloomFilter.contains("guojia"));//false
-        System.out.println(bloomFilter.contains("baiqi"));//false
-        System.out.println(bloomFilter.contains("zhuge"));//true
+        System.out.println(bloomFilter.contains("spengju"));//false
+        System.out.println(bloomFilter.contains("spj"));//false
+        System.out.println(bloomFilter.contains("peng"));//true
+        System.out.println(bloomFilter.contains("ju"));//true
     }
 }
